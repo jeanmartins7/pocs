ef6c82ebad6fd1ab0df431f8389c6b07661239a0
Jean Martins <jean.martins@sensedia.com>
2022-07-29 17:26:52 -0300

    start poc fluentd

---
+++ java/pocFluentd/PoCFluentd/src/main/java/com/martins/jean/pocfluentd/PoCFluentdApplication.java
@@ -0,0 +1,13 @@
+package com.martins.jean.pocfluentd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoCFluentdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoCFluentdApplication.class, args);
+	}
+
+}
---
+++ java/pocFluentd/PoCFluentd/src/test/java/com/martins/jean/pocfluentd/PoCFluentdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martins.jean.pocfluentd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoCFluentdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
