a0f77fd1e044a9e93277141ed30ce44c26de66b4
Jean Martins <jeanmarcel8@gmail.com>
2024-02-06 20:48:45 -0300

    start project kafka

---
+++ java/kafka-start-tutorials/src/test/java/kafkastarttutorials/martins/jean/kafkastarttutorials/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kafkastarttutorials.martins.jean.kafkastarttutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
